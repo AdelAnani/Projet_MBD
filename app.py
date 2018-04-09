f48ce1a1e3ab010b24a56888eab94ca8ccfeff5f
hamzalekkat <hemza.lekkat.1@ulaval.ca>
2018-04-09 18:56:40 -0400

    second commit , la route tracks

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, flash, redirect, url_for,session,logging,request
+from flask_mysqldb import  MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 from albums_repository import JsonAlbumsRepository
 from albums_service import AlbumsService
 
 app = Flask(__name__)
-
+#config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'glo2005'
+app.config['MYSQL_DB'] = 'myapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 # dependencies injection #
 album_repository = JsonAlbumsRepository("albums.json")
 albums_service = AlbumsService(album_repository)
@@ -40,7 +48,47 @@ def chanson(id):
     except:
         return render_template('not_found.html')
 
+class RegisterForm(Form):
+
+    name = StringField ('Name',[validators.length(min=1, max=50)])
+    username = StringField('Username', [validators.length(min=4, max=25)])
+    email = StringField('Email', [validators.length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message= 'Passwords do not match')
+    ])
+    confirm = PasswordField('confirm Password')
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
+                    (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'secret123'
     app.run(debug=True)
+
